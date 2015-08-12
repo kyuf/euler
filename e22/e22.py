88ec0f3efebf00e7b738e765a810b0948d5a61b4
kyuf <jeff.jl.wen@gmail.com>
2015-08-11 23:12:29 -0700

    updated merge sort algorithm

--- e22/e22.py
+++ e22/e22.py
@@ -1,41 +1,51 @@
 #euler problem 22
+"""
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+"""
 #opening given name txt file and spiltting accordingly
 with open("e22.txt", "r") as f:
     A = f.readline().split('","')
 
+#removing quotations
 for i in [0, -1]:
     A[i] = A[i].replace('"', "")
 
 #defining sorting functions
 def merge_sort(A):
-    if len(A) <= 1:
+    if len(A) < 2:
         return A
     mid = len(A) // 2
-    left = A[:mid]
-    right = A[mid:]
 
-    left = merge_sort(left)
-    right = merge_sort(right)
+    left = merge_sort(A[:mid])
+    right = merge_sort(A[mid:])
 
-    return merge(left, right)
-
-def merge(left, right):
-    result = []
-    while left and right:
+    i = 0
+    j = 0
+    k = 0
+    
+    while i < len(left) and j < len(right):
         #alpha will return whichever input comes first alphabetically
-        tmp = alpha(left[0], right[0])
-        result.append(tmp)
-        if tmp == left[0]:
-            del left[0]
+        tmp = alpha(left[i], right[j])
+        A[k] = tmp
+        if tmp == left[i]:
+            i += 1
         else:
-            del right[0]
-    while left:
-        result.append(left[0])
-        del left[0]
-    while right:
-        result.append(right[0])
-        del right[0]
-    return result
+            j += 1
+        k += 1
+        
+    while i < len(left):
+        A[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        A[k] = right[j]
+        j += 1
+        k += 1
+    return A
 
 def alpha(left, right):
     len_left = len(left)
@@ -58,5 +68,5 @@ for i in range(0, len(A)):
         tmp += (ord(j) - 64)
     total += ((i + 1) * tmp)
 
-print total
+print(total)
     
