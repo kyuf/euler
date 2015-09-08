9ddb54f2d277b8fda82d7ac20a653a92206fbd49
kyuf <jeff.jl.wen@gmail.com>
2015-09-08 00:46:12 -0700

    initial commit

---
+++ e20/e20.py
@@ -0,0 +1,28 @@
+#euler 20
+"""
+n! means n × (n − 1) * ... * 3 * 2 * 1
+
+For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+n = 1
+
+for i in range(2, 101):
+    n *= i
+    
+    #code below trims end zeros
+    """
+    while (n % 10) == 0:
+        n //= 10
+    """
+
+sum_n = 0
+
+for i in str(n):
+    sum_n += int(i)
+
+print(sum_n)
+
+
