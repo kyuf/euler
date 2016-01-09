a801590b85eb5edbb531a4fd1407f78adb811ac0
kyuf <jeff.jl.wen@gmail.com>
2016-01-09 15:13:10 -0800

    merged is_factor function into main code

--- e03/e03.py
+++ e03/e03.py
@@ -4,8 +4,6 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143?
 """
-limit = int(600851475143 ** 0.5)
-
 #prime function
 def is_prime(n, prime_list = []):
     for i in prime_list:
@@ -16,17 +14,13 @@ def is_prime(n, prime_list = []):
     prime_list.append(n)
     return True
 
-#factor function
-def is_factor(n, m = 600851475143):
-    if m % n == 0:
-        return True
-    else:
-        return False
-
+#finding largest prime factor of 600851475143
+m = 600851475143
+limit = int(m ** 0.5)
 max_n = 0
 for n in range(3, limit, 2):
     if is_prime(n):
-        if is_factor(n):
+        if m % n == 0:
             max_n = n
 
 print(max_n)
